e288609e17dfcc273f03ad637c64c32ca9176c74
Shifyan <asifyan01@gmail.com>
2024-08-03 12:42:08 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="body bg-[#111729] h-screen flex">
+        <div className="bg-[#97A3B6] ">
+          <div className="bg-[url('./assets/bg-image-random-quote.svg')] h-96 w-96"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
