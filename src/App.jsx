5f824fa16afaeb213e4c222846126fbb3bb86575
Shifyan <asifyan01@gmail.com>
2024-08-03 18:47:54 +0700

    content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 function App() {
   return (
     <>
-      <div className="body bg-[#111729] h-screen flex">
-        <div className="bg-[#97A3B6] ">
-          <div className="bg-[url('./assets/bg-image-random-quote.svg')] h-96 w-96"></div>
+      <div className="body ">
+        <div className="bg-[#20293A] inline-block">
+          <div className="bg-[url('./assets/bg-image-random-quote.svg')] bg-cover ">
+            <div className="content-body m-5">
+              <div className="figure-section text-[#97A3B6] text-lg font-semibold">
+                <h1>Shifyan Almusfaid</h1>
+              </div>
+              <div className="tag-section flex gap-3 text-[#6466E9]">
+                <h2 className="py-1 px-2 border rounded-2xl border-[#6466E9]">
+                  Quotes
+                </h2>
+                <h2 className="py-1 px-2 border rounded-2xl border-[#6466E9]">
+                  Inspirasi
+                </h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
